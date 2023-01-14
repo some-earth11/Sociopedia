ec81d47daa8f2a60fdef80f8b4c880448fe46468
some-earth11 <sk8762@srmist.edu.in>
2023-01-15 02:32:26 +0530

    configured mongodb

--- server/index.js
+++ server/index.js
@@ -35,3 +35,17 @@ const storage = multer.diskStorage({
         cb(null, file.originalname);
     }
 })
+
+const upload = multer({ storage });
+
+
+// MONGOOSE
+const PORT = process.env.PORT || 6001;
+mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(()=>{
+    app.listen(PORT, ()=> console.log(`Server Port: ${PORT}`))
+}).catch((error)=>{
+    console.log(`${error} did not connect`)
+})
