da18a05b8ded7fac6d93eb8c7ffe6e652ce75b86
some-earth11 <sk8762@srmist.edu.in>
2023-01-16 16:57:34 +0530

    react persist configured

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,19 @@
-import './App.css';
+import { useState } from 'react';
+import { BrowserRouter, Navigate, Routes, Route } from 'react-router-dom';
+import HomePage from 'scenes/homePage';
+import LoginPage from 'scenes/loginPage';
+import ProfilePage from 'scenes/profilePage';
 
 function App() {
   return (
     <div className="app">
-      
+      <BrowserRouter>
+        <Routes>
+          <Route path= "/" element={<LoginPage/>}/>
+          <Route path= "/home" element={<HomePage/>}/>
+          <Route path= "/profile/:userId" element={<ProfilePage/>}/>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
