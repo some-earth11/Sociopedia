039fd8ed2914e05b1e654ec69bf70e85b3d80216
some-earth11 <sk8762@srmist.edu.in>
2023-01-16 18:20:07 +0530

    navbar fixed

--- client/src/scenes/navbar/index.jsx
+++ client/src/scenes/navbar/index.jsx
@@ -39,8 +39,8 @@ const Navbar = () => {
     const primaryLight = theme.palette.primary.light;
     const alt = theme.palette.background.alt;
 
-    // const fullName = `${user.firstName} ${user.lastName}`;
-    const fullName = "Samarth Kumar";
+    const fullName = `${user.firstName} ${user.lastName}`;
+    // const fullName = "Samarth Kumar";
 
 
     return (
