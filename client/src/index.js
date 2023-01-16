9fd43b1e48c4a5dff8406ad282e5761175e4cb4c
some-earth11 <sk8762@srmist.edu.in>
2023-01-16 17:56:08 +0530

    Navbar component complete

--- client/src/index.js
+++ client/src/index.js
@@ -33,6 +33,10 @@ const store = configureStore({
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistStore(store)}>
+      <App />
+    </PersistGate>
+  </Provider>
+</React.StrictMode>
 );
