98223c953e58c26b904a667b3780e6a22636f400
some-earth11 <sk8762@srmist.edu.in>
2023-01-16 14:57:51 +0530

    inserted data

--- server/index.js
+++ server/index.js
@@ -14,6 +14,9 @@ import postRoutes from "./routes/posts.js";
 import { register } from "./controllers/auth.js";
 import { createPost } from "./controllers/posts.js";
 import { verifyToken } from "./middleware/auth.js";
+import User from "./models/User.js";
+import Post from "./models/Posts.js";
+import { users, posts } from "./data/index.js";
 
 // CONFIGURATIONS
 const __filename = fileURLToPath(import.meta.url);
@@ -61,6 +64,10 @@ mongoose.connect(process.env.MONGO_URL, {
     useUnifiedTopology: true,
 }).then(()=>{
     app.listen(PORT, ()=> console.log(`Server Port: ${PORT}`))
+
+    //ADD ONE TIME ONLY
+    // User.insertMany(users);
+    // Post.insertMany(posts);
 }).catch((error)=>{
     console.log(`${error} did not connect`)
 })
