bddd564826fe3208f38c79ef0156db9cc0b40424
some-earth11 <sk8762@srmist.edu.in>
2023-01-16 12:57:42 +0530

    user model created

---
+++ server/models/User.js
@@ -0,0 +1,44 @@
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema(
+    {
+        firstName: {
+            type: String,
+            require: true,
+            mon: 2,
+            max: 50,
+        },
+        lastName: {
+            type: String,
+            require: true,
+            min: 2,
+            max: 50,
+        },
+        email: {
+            type: String,
+            require: true,
+            max: 50,
+            unique: true,
+        },
+        password: {
+            type: String,
+            required: true,
+            min: 5,
+        },
+        picturePath: {
+            type: String,
+            default: "",
+        },
+        friends: {
+            type: Array,
+            default: [],
+        },
+        location: String,
+        occupation: String,
+        viewedProfile: Number,
+        impressions: Number,
+    },
+    { timestamps:true }
+);
+
+const User = mongoose.model("User", UserSchema)
