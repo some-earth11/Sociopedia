159e2f8cea1ec180601081a62e626d31c2266972
some-earth11 <sk8762@srmist.edu.in>
2023-01-16 13:57:48 +0530

    read, update routes added

--- server/index.js
+++ server/index.js
@@ -9,6 +9,7 @@ import dotenv from "dotenv";
 import path from "path";
 import { fileURLToPath } from "url";
 import authRoutes from "./routes/auth.js";
+import userRoutes from "./routes/users.js";
 import { register } from "./controllers/auth.js";
 
 // CONFIGURATIONS
@@ -46,6 +47,7 @@ app.post("/auth/register", upload.single("picture"), register);
 
 //ROUTES
 app.use("/auth", authRoutes);
+app.use("/users", userRoutes);
 
 // MONGOOSE
 const PORT = process.env.PORT || 6001;
