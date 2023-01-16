0daf92bfaf1b54acbecfa569acf134b6cccca47c
some-earth11 <sk8762@srmist.edu.in>
2023-01-16 14:26:57 +0530

    posts schema setup

---
+++ server/models/Posts.js
@@ -0,0 +1,35 @@
+import mongoose, { modelNames } from "mongoose";
+
+const PostSchema = mongoose.Schema(
+    {
+        userId: {
+            type: String,
+            required: true,
+        },
+        firstName: {
+            type: String,
+            required: true,
+        },
+        lastName: {
+            type: String,
+            required: true,
+        },
+        location: String,
+        description: String,
+        picturePath: String,
+        userPicturePath: String,
+        likes: {
+            type: Map,
+            of: Boolean,
+        },
+        comments: {
+            types: Array,
+            default : [],
+        }
+    },
+    { timestamps: true }
+);
+
+const Post = mongoose.model("Post", PostSchema);
+
+export default Post;
